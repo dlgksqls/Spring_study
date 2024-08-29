d5a477bec20853c59f0c1969ff0ee62f4b757aad
dlgksqls <horainlee87@gmail.com>
2024-08-29 21:23:16 +0900

    study

--- hello/src/main/java/hello/servlet/web/frontcontroller/MyView.java
+++ hello/src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.Map;
 
 public class MyView {
     private String viewPath;
@@ -14,8 +15,13 @@ public class MyView {
         this.viewPath = viewPath;
     }
 
-    public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+    public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        ModelToRequestAttribute(model, request);
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request, response);
+    }
+
+    public void ModelToRequestAttribute(Map<String, Object> model, HttpServletRequest request) throws ServletException, IOException{
+        model.forEach((key, value) -> request.setAttribute(key, value));
     }
 }
--- hello/src/main/java/hello/servlet/web/frontcontroller/v2/FrontControllerServletV2.java
+++ hello/src/main/java/hello/servlet/web/frontcontroller/v2/FrontControllerServletV2.java
@@ -44,6 +44,6 @@ public class FrontControllerServletV2 extends HttpServlet {
         }
 
         MyView view = controller.process(request, response);
-        view.render(request, response);
+        //view.render(request, response);
     }
 }
