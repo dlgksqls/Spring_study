992d4b54db110e3e02bb4e7630886b7a5507e4fa
dlgksqls <horainlee87@gmail.com>
2024-08-31 21:13:11 +0900

    study

---
+++ hello/src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -0,0 +1,20 @@
+package hello.servlet.web.springmvc.old;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+// 스프링 빈 이름을 url로 맞춤
+@Component("/springmvc/old-controller")
+public class OldController implements Controller {
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("OldController.handleRequest");
+
+        // viewresolver
+        return new ModelAndView("new-form");
+    }
+}
