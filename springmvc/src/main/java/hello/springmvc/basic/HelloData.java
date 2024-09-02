7d2704fda7b54f655676bc57218a31d0cedc353a
dlgksqls <horainlee87@gmail.com>
2024-09-02 21:51:02 +0900

    study

---
+++ springmvc/src/main/java/hello/springmvc/basic/HelloData.java
@@ -0,0 +1,11 @@
+package hello.springmvc.basic;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+public class HelloData {
+    private String username;
+    private int age;
+}
