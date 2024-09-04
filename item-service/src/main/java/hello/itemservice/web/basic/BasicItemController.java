7758c4f4164ad0f3f931aaa6c025f5558b8e0546
dlgksqls <horainlee87@gmail.com>
2024-09-04 21:11:55 +0900

    study

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -7,9 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,6 +28,76 @@ public class BasicItemController {
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
         return "basic/items";
+    }
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable Long itemId, Model model){
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+    @GetMapping("/add")
+    public String addForm(){
+        return"basic/addForm";
+    }
+    //@PostMapping("/add")
+    public String addItemV1(
+            @RequestParam String itemName,
+            @RequestParam int price,
+            @RequestParam int quantity,
+            Model model
+    ){
+        Item item = new Item(itemName, price, quantity);
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+//    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item, Model model){
+
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+    //@PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item) {
+
+        // Item -> item
+        // HelloData -> hellodata
+        // 클래스의 앞 글자만 소문자로 바뀌어서 저장됨
+
+        // model.addAttribute("helloData", item) 이렇게 됨....
+
+        itemRepository.save(item);
+
+        return "basic/item";
+    }
+    @PostMapping("/add")
+    public String addItemV4(Item item){
+
+        // Item -> item
+        // HelloData -> hellodata
+        // 클래스의 앞 글자만 소문자로 바뀌어서 저장됨
+
+        // model.addAttribute("helloData", item) 이렇게 됨....
+
+        itemRepository.save(item);
+
+        return "basic/item";
+    }
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model){
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+    @PostMapping("{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item){
+        itemRepository.update(itemId, item);
+        return
     }
 
     @PostConstruct
