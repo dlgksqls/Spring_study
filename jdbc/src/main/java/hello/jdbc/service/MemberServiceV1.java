d333c7a43adf0a08ec7e71b7e746a9c84b0b9cf8
dlgksqls <horainlee87@gmail.com>
2024-09-09 11:36:43 +0900

    study

---
+++ jdbc/src/main/java/hello/jdbc/service/MemberServiceV1.java
@@ -0,0 +1,35 @@
+package hello.jdbc.service;
+
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV1;
+import lombok.RequiredArgsConstructor;
+
+import java.sql.SQLException;
+
+@RequiredArgsConstructor
+public class MemberServiceV1 {
+
+    private final MemberRepositoryV1 memberRepository;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        /**
+         * 같은 커넥션(= 하나의 트랜잭션)을 유지해야한다.
+         */
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+
+        validation(toMember);
+
+        memberRepository.update(toId, toMember.getMoney() + money);
+    }
+
+    private static void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")){
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+}
+
