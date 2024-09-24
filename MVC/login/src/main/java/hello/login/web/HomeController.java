f93c58562b1c1d6ed80848ab97b4b7f194739547
dlgksqls <horainlee87@gmail.com>
2024-09-24 21:35:05 +0900

    study

--- MVC/login/src/main/java/hello/login/web/HomeController.java
+++ MVC/login/src/main/java/hello/login/web/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -58,7 +59,7 @@ public class HomeController {
         return "loginHome";
     }
 
-    @GetMapping("/")
+    //@GetMapping("/")
     public String homeLogin3(HttpServletRequest request, Model model){
 
         HttpSession session = request.getSession(false); // 로그인 하지 않은 사용자도 들어오므로 처음에 생성하면 안됨
@@ -68,6 +69,20 @@ public class HomeController {
 
         Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
 
+        // 세션에 회원 데이터가 없으면 home
+        if(loginMember == null){
+            return "home";
+        }
+
+        // 세션 유지되면 로그인으로 이동
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+    @GetMapping("/")
+    public String homeLogin3Spring(
+            @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model
+    ){
         // 세션에 회원 데이터가 없으면 home
         if(loginMember == null){
             return "home";
