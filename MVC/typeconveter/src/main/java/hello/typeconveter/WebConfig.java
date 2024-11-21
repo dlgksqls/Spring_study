88ce6893dd645dc5050d003311102ff502742cb1
dlgksqls <horainlee87@gmail.com>
2024-11-21 11:52:03 +0900

    study

--- MVC/typeconveter/src/main/java/hello/typeconveter/WebConfig.java
+++ MVC/typeconveter/src/main/java/hello/typeconveter/WebConfig.java
@@ -4,6 +4,7 @@ import hello.typeconveter.converter.IntegerToStringConverter;
 import hello.typeconveter.converter.IpPortToStringConverter;
 import hello.typeconveter.converter.StringToIntegerConverter;
 import hello.typeconveter.converter.StringToIpPortConverter;
+import hello.typeconveter.formatter.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,11 +14,15 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new StringToIntegerConverter());
-        registry.addConverter(new IntegerToStringConverter());
+        // 주석 처리 우선순위 때문 (컨버터 > 포매터)
+//        registry.addConverter(new StringToIntegerConverter());
+//        registry.addConverter(new IntegerToStringConverter());
 
         // 이것 두개는 커스텀해서 등록해줘야함
         registry.addConverter(new StringToIpPortConverter());
         registry.addConverter(new IpPortToStringConverter());
+
+        // 추가
+        registry.addFormatter(new MyNumberFormatter());
     }
 }
