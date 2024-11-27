c326baafd8fdf924dcd16e84c8608fc726bca782
dlgksqls <horainlee87@gmail.com>
2024-11-27 12:53:26 +0900

    study

--- Advenced/proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ Advenced/proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,9 +1,14 @@
 package hello.proxy;
 
+import hello.proxy.config.AppV1Config;
+import hello.proxy.config.AppV2Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
+//@Import(AppV1Config.class)
+@Import({AppV1Config.class, AppV2Config.class})
+@SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의 (app 패키지 하위만 컴포넌트 스캔을 함), 그래서 위에 import를 함
 public class ProxyApplication {
 
 	public static void main(String[] args) {
