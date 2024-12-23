f37b9207122ac45c4937aca8eb5b8c224555b31e
dlgksqls <horainlee87@gmail.com>
2024-12-23 18:10:55 +0900

    study

--- Security/springSecurityMaster/src/main/java/io/secyruty/springsecuritymaster/SecurityConfig.java
+++ Security/springSecurityMaster/src/main/java/io/secyruty/springsecuritymaster/SecurityConfig.java
@@ -1,14 +1,23 @@
 package io.secyruty.springsecuritymaster;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
 
 @EnableWebSecurity
 @Configuration
@@ -17,10 +26,28 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http
-                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-                .httpBasic(basic -> basic.authenticationEntryPoint(new CustomAuthenticationEntryPoint())
-                );
+        http.formLogin(auth -> auth
+                .loginPage("/loginPage")
+                .loginProcessingUrl("/loginProc")
+                .defaultSuccessUrl("/", true)  // true 면 무조건 지정한 위치로 이동함 false 면 이전 위치로 이동함
+                .failureUrl("/failed")
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .successHandler(new AuthenticationSuccessHandler() {
+                    @Override
+                    public void onAuthenticationSuccess(HttpServletRequest request,
+                                                        HttpServletResponse response,
+                                                        Authentication authentication)
+                            throws IOException, ServletException {
+                        System.out.println("authentication : " + authentication);
+                        response.sendRedirect("/home");
+                    }
+                })
+                .failureHandler((request, response, exception) -> {
+                    System.out.println("exception : " + exception.getMessage());
+                    response.sendRedirect("/login");
+                })
+        );
 
         return http.build();
     }
