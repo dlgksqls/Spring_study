06be6a07755105211d9ec59895d5ecaee4691572
dlgksqls <horainlee87@gmail.com>
2025-03-09 02:22:58 +0900

    test 공부

---
+++ Test/test-code-with-architecture-main/src/main/java/com/example/demo/post/controller/PostController.java
@@ -0,0 +1,38 @@
+package com.example.demo.post.controller;
+
+import com.example.demo.user.controller.UserController;
+import com.example.demo.post.controller.response.PostResponse;
+import com.example.demo.post.domain.PostUpdate;
+import com.example.demo.post.service.PostService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "게시물(posts)")
+@RestController
+@RequestMapping("/api/posts")
+@RequiredArgsConstructor
+public class PostController {
+
+    private final PostService postService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PostResponse> getPostById(@PathVariable long id) {
+        return ResponseEntity
+            .ok()
+            .body(PostResponse.from(postService.getById(id)));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PostResponse> updatePost(@PathVariable long id, @RequestBody PostUpdate postUpdate) {
+        return ResponseEntity
+            .ok()
+            .body(PostResponse.from(postService.update(id, postUpdate)));
+    }
+}
---
+++ Test/test-code-with-architecture-main/src/test/java/com/example/demo/user/infrastructure/UserJpaRepositoryTest.java
@@ -0,0 +1,72 @@
+package com.example.demo.user.infrastructure;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import com.example.demo.user.domain.User;
+import com.example.demo.user.domain.UserStatus;
+import java.util.Optional;
+
+import com.example.demo.user.service.port.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+//@DataJpaTest(showSql = true)
+@SpringBootTest
+@TestPropertySource("classpath:test-application.properties")
+@SqlGroup({
+        @Sql(value = "/sql/user-service-test-data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD),
+        @Sql(value = "/sql/delete-all-data.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+})
+public class UserJpaRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void findByIdAndStatus_로_유저_데이터를_찾아올_수_있다() {
+        // given
+        // when
+        Optional<User> result = userRepository.findByIdAndStatus(1, UserStatus.ACTIVE);
+
+        // then
+        assertThat(result.isPresent()).isTrue();
+    }
+
+    @Test
+    void findByIdAndStatus_는_데이터가_없으면_Optional_empty_를_내려준다() {
+        // given
+        // when
+        Optional<User> result = userRepository.findByIdAndStatus(1, UserStatus.PENDING);
+
+        // then
+        assertThat(result.isEmpty()).isTrue();
+    }
+
+    @Test
+    void findByEmailAndStatus_로_유저_데이터를_찾아올_수_있다() {
+        // given
+        // when
+        Optional<User> result = userRepository.findByEmailAndStatus("testemail393939@gmail.com", UserStatus.ACTIVE);
+
+        // then
+        assertThat(result.isPresent()).isTrue();
+    }
+
+    @Test
+    void findByEmailAndStatus_는_데이터가_없으면_Optional_empty_를_내려준다() {
+        // given
+        // when
+        Optional<User> result = userRepository.findByEmailAndStatus("testemail393939@gmail.com", UserStatus.PENDING);
+
+        // then
+        assertThat(result.isEmpty()).isTrue();
+    }
+}
