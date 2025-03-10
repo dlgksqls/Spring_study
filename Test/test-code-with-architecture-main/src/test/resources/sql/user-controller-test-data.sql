97703e28786a5cae3e5dcc16ad62dfcb4ce4e2d0
dlgksqls <horainlee87@gmail.com>
2025-03-10 20:10:31 +0900

    study

--- Test/test-code-with-architecture-main/src/test/resources/sql/user-controller-test-data.sql
+++
@@ -1,5 +0,0 @@
-insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
-values (1, 'testemail393939@gmail.com', 'test', 'test', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'ACTIVE', 0);
-
-insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
-values (2, 'testemail39393939@gmail.com', 'test', 'test', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaab', 'PENDING', 0);
--- Test/test-code-with-architecture-main/src/test/resources/sql/user-repository-test-data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO users (id, email, nickname, address, certification_code, status, last_login_at)
-VALUES (1, 'testemail393939@gmail.com', 'test', 'test', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'ACTIVE', 0);
